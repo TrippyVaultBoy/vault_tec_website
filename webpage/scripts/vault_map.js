b30d8287960c43e59de5a258c9775e4aedbffe72
Tanner Saint <tsaint2004@gmail.com>
2025-01-18 07:52:33 +0000

    added basic map interaction

--- webpage/scripts/vault_map.js
+++ webpage/scripts/vault_map.js
@@ -18,26 +18,44 @@ const data = {
     vault_111: {
         coords: [42.469778, -71.354581],
         title: "Vault 111",
+        location: "Sanctuary Hills, MA",
+        capacity: "35",
+        power: "Vault Star Super-reactor",
     },
     vault_101: {
         coords: [38.9258, -77.1738],
-        title: "Vault 101"
+        title: "Vault 101",
+        location: "Springvale, VA",
+        capacity: "300",
+        power: "General Atomics nuclear power",
     },
     vault_76: {
         coords: [39.6406, -80.5923],
-        title: "Vault 76"
+        title: "Vault 76",
+        location: "Flatwoods, WV",
+        capacity: "88",
+        power: "Vault Star Super-reactor",
     },
     vault_33: {
         coords: [34.008985, -118.493155],
-        title: "Vault 33"
+        title: "Vault 33",
+        location: "Santa Monica, CA",
+        capacity: "500",
+        power: "General Atomics nuclear power",
     },
     vault_21: {
         coords: [36.135466, -115.151310],
-        title: "Vault 21"
+        title: "Vault 21",
+        location: "Las Vegas, NV",
+        capacity: "220",
+        power: "General Atomics nuclear power",
     },
     vault_13: {
         coords: [36.57851, -118.29213],
-        title: "Vault 13"
+        title: "Vault 13",
+        location: "Mt. Whitney,",
+        capacity: "500",
+        power: "Geothermal",
     }
 }
 
@@ -46,10 +64,22 @@ for (let key in data) {
 
     const vault = data[key];
 
-    L.marker(vault.coords, {
-    title: vault.title,
-    icon: customIcon
+    const marker = L.marker(vault.coords, {
+        title: vault.title,
+        icon: customIcon
     })
     .bindPopup(`<h2>${vault.title}</h2>`)
-    .addTo(map)
+    .addTo(map);
+
+    marker.on('click', function() {
+        document.getElementById('vault_title').textContent = vault.title;
+        document.getElementById('info_grid').innerHTML = `
+            <h2>Location:</h2>
+            <h3>${vault.location}</h3>
+            <h2>Capacity:</h2>
+            <h3>${vault.capacity}</h3>
+            <h2>Power Supply:</h2>
+            <h3>${vault.power}</h3>
+        `;
+    });
 }
